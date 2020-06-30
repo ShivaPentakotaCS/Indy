92603ebf31a16047b9624571dcf61152a08d8aab
AustinHolbrook <67373891+AustinHolbrook@users.noreply.github.com>
2020-06-30 14:50:18 -0400

    Updated Navbars

---
+++ Sports_Team01/Pages/ColtsLive.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sports_Team01.Pages
+{
+    public class ColtsLiveModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
